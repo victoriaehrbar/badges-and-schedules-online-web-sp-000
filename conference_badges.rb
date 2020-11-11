86b8f652eb1fed95ee89f5f1de5ff7961916e499
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-11 19:32:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+attendees = ["Bob", "John", "Martha"]
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+end
+end
